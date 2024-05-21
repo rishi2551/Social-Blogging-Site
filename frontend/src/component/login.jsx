b9468fe0f2ad40d38165c45225504a5f0ef2d518
rishisam2551 <rishisambhi941@gmail.com>
2024-05-21 16:01:13 +0530

    change

--- frontend/src/component/login.jsx
+++ frontend/src/component/login.jsx
@@ -26,7 +26,6 @@ const Login = () => {
       if (res.data.token) {
         localStorage.setItem("token", res.data.token);
         localStorage.setItem("userId",res.data.userId);
-        
         navigate("/main");
       }
     }
